06ea89bfc958bba009941f9222ebaf5d8c73f036
rkervella <rkervella@bishopfox.com>
2019-02-21 21:28:15 +0100

    Fixed merge errors

--- client/command/init.go
+++ client/command/init.go
@@ -481,6 +481,12 @@ func Init(app *grumble.App, rpc RPCServer) {
 		Run: func(ctx *grumble.Context) error {
 			fmt.Println()
 			getsystem(ctx, rpc)
+			fmt.Println()
+			return nil
+		},
+	})
+
+	app.AddCommand(&grumble.Command{
 		Name:      consts.ExecuteAssemblyStr,
 		Help:      "Load and executes a .NET assembly in a child process",
 		LongHelp:  help.GetHelpFor(consts.ExecuteAssemblyStr),
