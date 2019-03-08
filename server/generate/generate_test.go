82a49fbb9b28ef0e23aaf9a2d6313b4bcf4edf67
moloch <moloch38@gmail.com>
2019-03-07 17:59:47 -0800

    connection successful, commands timeout rn

--- server/generate/generate_test.go
+++ server/generate/generate_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSliverExecutableWindows64Bit(t *testing.T) {
+func TestSliverC2s(t *testing.T) {
 
 	// mTLS C2
 	mtls(t, "windows", "amd64", false)
@@ -19,38 +19,25 @@ func TestSliverExecutableWindows64Bit(t *testing.T) {
 	dns(t, "windows", "amd64", true)
 	dns(t, "windows", "386", true)
 
+	// HTTP C2
+	http(t, "windows", "amd64", false)
+	http(t, "windows", "386", false)
+	http(t, "windows", "amd64", true)
+	http(t, "windows", "386", true)
+
+	// Multiple C2s
+	multiC2(t, "windows", "amd64", true)
+	multiC2(t, "windows", "amd64", false)
 }
 
 func TestSliverExecutableLinux(t *testing.T) {
-
-	// mTLS C2
-	mtls(t, "linux", "amd64", false)
-	mtls(t, "linux", "386", false)
-	mtls(t, "linux", "amd64", true)
-	mtls(t, "linux", "386", true)
-
-	// DNS C2
-	dns(t, "linux", "amd64", false)
-	dns(t, "linux", "386", false)
-	dns(t, "linux", "amd64", true)
-	dns(t, "linux", "386", true)
-
+	multiC2(t, "linux", "amd64", true)
+	multiC2(t, "linux", "amd64", false)
 }
 
 func TestSliverExecutableDarwin(t *testing.T) {
-
-	// mTLS C2
-	mtls(t, "darwin", "amd64", false)
-	mtls(t, "darwin", "386", false)
-	mtls(t, "darwin", "amd64", true)
-	mtls(t, "darwin", "386", true)
-
-	// DNS C2
-	dns(t, "darwin", "amd64", false)
-	dns(t, "darwin", "386", false)
-	dns(t, "darwin", "amd64", true)
-	dns(t, "darwin", "386", true)
-
+	multiC2(t, "darwin", "amd64", true)
+	multiC2(t, "darwin", "amd64", false)
 }
 
 func mtls(t *testing.T, goos string, goarch string, debug bool) {
@@ -75,6 +62,38 @@ func dns(t *testing.T, goos string, goarch string, debug bool) {
 		GOARCH:    goarch,
 		DNSParent: "example.com",
 		Debug:     debug,
+	}
+	_, err := SliverExecutable(config)
+	if err != nil {
+		t.Errorf(fmt.Sprintf("%v", err))
+	}
+}
+
+func http(t *testing.T, goos string, goarch string, debug bool) {
+	t.Logf("[http] %s/%s - debug: %v", goos, goarch, debug)
+	config := &SliverConfig{
+		GOOS:       goos,
+		GOARCH:     goarch,
+		HTTPServer: "example.com",
+		Debug:      debug,
+	}
+	_, err := SliverExecutable(config)
+	if err != nil {
+		t.Errorf(fmt.Sprintf("%v", err))
+	}
+}
+
+func multiC2(t *testing.T, goos string, goarch string, debug bool) {
+	t.Logf("[multi] %s/%s - debug: %v", goos, goarch, debug)
+	config := &SliverConfig{
+		GOOS:   goos,
+		GOARCH: goarch,
+
+		MTLSServer: "1.example.com",
+		MTLSLPort:  1337,
+		HTTPServer: "2.example.com",
+		DNSParent:  "3.example.com",
+		Debug:      debug,
 	}
 	_, err := SliverExecutable(config)
 	if err != nil {
