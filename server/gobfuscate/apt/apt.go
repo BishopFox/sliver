f3797501f00ca58f0c7dc97af2c9ee81454b51a8
moloch <moloch38@gmail.com>
2019-05-28 20:51:59 -0500

    Added apt char sets

--- server/gobfuscate/apt/apt.go
+++ server/gobfuscate/apt/apt.go
@@ -16,9 +16,9 @@ func RandomString(group int) string {
 
 // APTGroups - Map of groups to character sets
 var APTGroups = map[int][]string{
-	1:  chinese,
-	3:  chinese,
-	5:  undisclosed,
+	1: chinese,
+	3: chinese,
+	// 5:  undisclosed,
 	10: chinese,
 	12: chinese,
 	16: chinese,
@@ -37,9 +37,8 @@ var APTGroups = map[int][]string{
 	40: chinese,
 }
 
-var undisclosed = []string{}
-var chinese = []string{"隐蔽"}
-var iranian = []string{}
-var korean = []string{}
-var russian = []string{}
-var vietnamese = []string{}
+var chinese = []string{"隐蔽", "中华人民共和国", "义勇军进行曲", "上海市", "贝壳", "码", "鼠"}
+var iranian = []string{"اسلامی ایران‎", "استقلال، آزادی، جمهوری اسلامی", "سرود ملی جمهوری اسلامی ایران"}
+var korean = []string{"조선민주주의인민공화국", "애국가", "조선", "평양직할시", "량강도", "김정일"}
+var russian = []string{"Росси́йская", "Федера́ция", "Государственный", "гимн", "Российской", "Федерации", "подробнее", "рождений"}
+var vietnamese = []string{"nhớp", "nhác", "nương", "đâu", "Chuột", "Cửa hậu"}
