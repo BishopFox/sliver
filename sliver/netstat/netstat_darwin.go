9f9b3d3b4a21b5f84ffcbf35949bc323e37504be
moloch <moloch38@gmail.com>
2019-06-26 10:58:48 -0500

    added api files for darwin

---
+++ sliver/netstat/netstat_darwin.go
@@ -0,0 +1,21 @@
+package netstat
+
+var skStates = [...]string{
+	"UNKNOWN",
+}
+
+func osTCPSocks(accept AcceptFn) ([]SockTabEntry, error) {
+	return nil, nil
+}
+
+func osTCP6Socks(accept AcceptFn) ([]SockTabEntry, error) {
+	return nil, nil
+}
+
+func osUDPSocks(accept AcceptFn) ([]SockTabEntry, error) {
+	return nil, nil
+}
+
+func osUDP6Socks(accept AcceptFn) ([]SockTabEntry, error) {
+	return nil, nil
+}
