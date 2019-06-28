de628e2ad83c52a2c9dd4dfa449cb115604178e6
moloch <moloch38@gmail.com>
2019-06-27 22:38:34 -0500

    Fixed endian length issue

--- gui/rpc.ts
+++ gui/rpc.ts
@@ -85,7 +85,7 @@ export class RPCClient {
   private toBytesUint32(num: number): Buffer {
     const arr = new ArrayBuffer(4); // an Int32 takes 4 bytes
     const view = new DataView(arr);
-    view.setUint32(0, num, false); // byteOffset = 0; litteEndian = false
+    view.setUint32(0, num, true); // byteOffset = 0; litteEndian = true
     return Buffer.from(arr);
   }
 
