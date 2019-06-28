c6e70ce3d75eae332e4e2eeae9314ee2bf847721
moloch <moloch38@gmail.com>
2019-06-27 22:33:13 -0500

    Added gplv3

--- gui/rpc.ts
+++ gui/rpc.ts
@@ -1,3 +1,18 @@
+/*
+  Sliver Implant Framework
+  Copyright (C) 2019  Bishop Fox
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { Subject, Observable, Observer } from 'rxjs';
 import { TLSSocket, ConnectionOptions, TlsOptions, connect } from 'tls';
 import * as pb from './pb/sliver_pb';
