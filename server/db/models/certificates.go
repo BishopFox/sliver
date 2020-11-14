01f1c2c7d07fba7bac33ea3b5cecd18ceb0bf032
moloch-- <875022+moloch--@users.noreply.github.com>
2020-11-14 10:15:19 -0600

    Added constraints to certificate model

--- server/db/models/certificates.go
+++ server/db/models/certificates.go
@@ -4,15 +4,26 @@ import (
 	"time"
 
 	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
 )
 
 // Certificate - Certificate database model
 type Certificate struct {
-	ID             uuid.UUID `gorm:"->;type:uuid;"`
-	CreatedAt      time.Time `gorm:"->;"`
-	CommonName     string
+	ID             uuid.UUID `gorm:"gorm:"->;<-:create;type:uuid;unique;"`
+	CreatedAt      time.Time `gorm:"gorm:"->;<-:create;"`
+	CommonName     string    `gorm:"index:unique;"`
 	CAType         string
 	KeyType        string
 	CertificatePEM string
 	PrivateKeyPEM  string
+}
+
+// BeforeCreate - GORM hook to automatically set values
+func (c *Certificate) BeforeCreate(tx *gorm.DB) (err error) {
+	c.ID, err = uuid.NewV4()
+	if err != nil {
+		return err
+	}
+	c.CreatedAt = time.Now()
+	return nil
 }
