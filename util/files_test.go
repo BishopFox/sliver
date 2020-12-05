875decb7be1ed3a977200f64183b8d49a39b3523
moloch-- <875022+moloch--@users.noreply.github.com>
2020-12-05 17:43:11 -0600

    Removed dead code issue #256

--- util/files_test.go
+++ util/files_test.go
@@ -19,52 +19,11 @@ package util
 */
 
 import (
-	"bytes"
 	"crypto/rand"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"testing"
 )
 
 func randomData() []byte {
 	buf := make([]byte, 128)
 	rand.Read(buf)
 	return buf
-}
-
-func TestCopyFileContents(t *testing.T) {
-	sample := randomData()
-	tmpfile, err := ioutil.TempFile("", "sliver-unit-test")
-	if err != nil {
-		t.Error(err)
-	}
-	defer os.Remove(tmpfile.Name()) // clean up
-	if _, err := tmpfile.Write(sample); err != nil {
-		t.Error(err)
-	}
-	err = tmpfile.Close()
-	if err != nil {
-		t.Error(err)
-	}
-
-	dst := fmt.Sprintf("%s.2", tmpfile.Name())
-	CopyFileContents(tmpfile.Name(), dst)
-
-	srcData, err := ioutil.ReadFile(tmpfile.Name())
-	if err != nil {
-		t.Error(err)
-	}
-	dstData, err := ioutil.ReadFile(dst)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !bytes.Equal(sample, srcData) {
-		t.Error(fmt.Errorf("Sample and src do not match:\nsample: %v\n src: %v", sample, srcData))
-	}
-	if !bytes.Equal(dstData, srcData) {
-		t.Error(fmt.Errorf("dst and src do not match:\ndst: %v\nsrc: %v", dstData, srcData))
-	}
-
 }
