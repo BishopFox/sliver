e13c282f757ca7d50d0b1aef1332135c916eace1
maxlandon <maximelandon@gmail.com>
2021-01-28 13:57:13 -0500

    Fix sliverpb filenames when building implant.

--- server/assets/assets.go
+++ server/assets/assets.go
@@ -185,8 +185,8 @@ func SetupGoPath(goPathSrc string) error {
 	}
 	sliverpbDir := path.Join(goPathSrc, "github.com", "bishopfox", "sliver", "protobuf", "sliverpb")
 	os.MkdirAll(sliverpbDir, 0700)
-	ioutil.WriteFile(path.Join(sliverpbDir, "constants.go"), sliverpbGoSrc, 0644)
-	ioutil.WriteFile(path.Join(sliverpbDir, "sliver.pb.go"), sliverpbConstSrc, 0644)
+	ioutil.WriteFile(path.Join(sliverpbDir, "constants.go"), sliverpbConstSrc, 0644)
+	ioutil.WriteFile(path.Join(sliverpbDir, "sliver.pb.go"), sliverpbGoSrc, 0644)
 
 	// Common PB
 	commonpbSrc, err := protobufs.FS.ReadFile("commonpb/common.pb.go")
