279211a6c7b04ede15a2d574e655ae456077eb82
maxlandon <maximelandon@gmail.com>
2021-03-31 17:20:58 -0400

    Comps

--- client/completers/argument-lists.go
+++ client/completers/argument-lists.go
@@ -69,90 +69,13 @@ var (
 		"windows/arm",
 	}
 
-	// All of these architectures work with a simple Go compiler, no need for external ones.
-	// validArchsGoNative = map[string][]string{
-	//         "aix":       []string{"ppc64"},
-	//         "android":   []string{"x86", "amd64", "arm", "arm64"},
-	//         "darwin":    []string{"amd64", "arm64"},
-	//         "dragonfly": []string{"amd64", "arm64"},
-	//         "freebsd":   []string{"x86", "amd64", "arm", "arm64"},
-	//         "illumos":   []string{"amd64"},
-	//         "ios":       []string{"amd64", "arm64"},
-	//         "js":        []string{"wasm"},
-	//         "linux":     []string{"x86", "amd64", "arm", "arm64", "mips", "mips64", "mips64le", "mipsle", "ppc64", "ppc64le", "riscv64", "s390x"},
-	//         "netbsd":    []string{"x86", "amd64", "arm", "arm64"},
-	//         "openbsd":   []string{"x86", "amd64", "arm", "arm64", "mips64"},
-	//         "plan9":     []string{"x86", "amd64", "arm"},
-	//         "solaris":   []string{"amd64"},
-	//         "windows":   []string{"x86", "amd64", "arm"},
-	// }
-
 	// Stagers
 	stageListenerProtocols = []string{"tcp", "http", "https"}
 
 	// Assembly architectures
 	assemblyArchs = []string{"x86", "x64", "x84"}
 
-	// MSF
-	msfStagerProtocols  = []string{"tcp", "http", "https"}
-	msfTransformFormats = []string{
-		"bash",
-		"c",
-		"csharp",
-		"dw",
-		"dword",
-		"hex",
-		"java",
-		"js_be",
-		"js_le",
-		"num",
-		"perl",
-		"pl",
-		"powershell",
-		"ps1",
-		"py",
-		"python",
-		"raw",
-		"rb",
-		"ruby",
-		"sh",
-		"vbapplication",
-		"vbscript",
-	}
-
-	msfEncoders = []string{
-		"x86/shikata_ga_nai",
-		"x64/xor_dynamic",
-	}
-
-	msfPayloads = map[string][]string{
-		"windows": windowsMsfPayloads,
-		"linux":   linuxMsfPayloads,
-		"osx":     osxMsfPayloads,
-	}
-
-	// ValidPayloads - Valid payloads and OS combos
-	windowsMsfPayloads = []string{
-		"meterpreter_reverse_http",
-		"meterpreter_reverse_https",
-		"meterpreter_reverse_tcp",
-		"meterpreter/reverse_tcp",
-		"meterpreter/reverse_http",
-		"meterpreter/reverse_https",
-	}
-	linuxMsfPayloads = []string{
-		"meterpreter_reverse_http",
-		"meterpreter_reverse_https",
-		"meterpreter_reverse_tcp",
-	}
-	osxMsfPayloads = []string{
-		"meterpreter_reverse_http",
-		"meterpreter_reverse_https",
-		"meterpreter_reverse_tcp",
-	}
-
 	// Comm network protocols
-	portfwdProtocols     = []string{"tcp", "udp"}
-	transportProtocols   = []string{"tcp", "udp", "ip"}
-	applicationProtocols = []string{"http", "https", "mtls", "quic", "http3", "dns", "named_pipe"}
+	portfwdProtocols   = []string{"tcp", "udp"}
+	transportProtocols = []string{"tcp", "udp", "ip"}
 )
