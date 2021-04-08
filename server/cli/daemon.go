f61648c62d431c0f250f6154cea945337a5ee82d
moloch-- <875022+moloch--@users.noreply.github.com>
2021-04-08 08:51:11 -0500

    Perform standard asset init steps on daemon mode start

--- server/cli/daemon.go
+++ server/cli/daemon.go
@@ -1,6 +1,15 @@
 package cli
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"runtime/debug"
+
+	"github.com/bishopfox/sliver/server/assets"
+	"github.com/bishopfox/sliver/server/c2"
+	"github.com/bishopfox/sliver/server/certs"
+	"github.com/bishopfox/sliver/server/configs"
 	"github.com/bishopfox/sliver/server/daemon"
 	"github.com/spf13/cobra"
 )
@@ -10,6 +19,26 @@ var daemonCmd = &cobra.Command{
 	Short: "Start server in daemon mode",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+
+		appDir := assets.GetRootAppDir()
+		logFile := initLogging(appDir)
+		defer logFile.Close()
+
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("panic:\n%s", debug.Stack())
+				fmt.Println("stacktrace from panic: \n" + string(debug.Stack()))
+				os.Exit(99)
+			}
+		}()
+
+		assets.Setup(false)
+		certs.SetupCAs()
+		certs.SetupWGKeys()
+
+		serverConfig := configs.GetServerConfig()
+		c2.StartPersistentJobs(serverConfig)
+
 		daemon.Start()
 	},
 }
