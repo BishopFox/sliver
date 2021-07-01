330e0e7bddc0b84bb4f2e1f7e06153d7ecb8e0e2
moloch-- <875022+moloch--@users.noreply.github.com>
2021-07-01 18:07:00 -0500

    Added missing lic headers

--- client/command/priv.go
+++ client/command/priv.go
@@ -1,5 +1,23 @@
 package command
 
+/*
+	Sliver Implant Framework
+	Copyright (C) 2021  Bishop Fox
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import (
 	"context"
 	"fmt"
@@ -47,7 +65,7 @@ func runAs(ctx *grumble.Context, rpc rpcpb.SliverRPCClient) {
 		return
 	}
 
-	fmt.Printf(Info+"Sucessfully ran %s %s on %s\n", process, arguments, session.GetName())
+	fmt.Printf(Info+"Successfully ran %s %s on %s\n", process, arguments, session.GetName())
 }
 
 func impersonate(ctx *grumble.Context, rpc rpcpb.SliverRPCClient) {
