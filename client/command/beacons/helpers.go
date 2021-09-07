83adb36bbdb0525db5e64b8e85f31df29edd0722
moloch-- <875022+moloch--@users.noreply.github.com>
2021-09-06 20:43:06 -0500

    Beacon rm tweaks

--- client/command/beacons/helpers.go
+++ client/command/beacons/helpers.go
@@ -1,5 +1,23 @@
 package beacons
 
+/*
+	Sliver Implant Framework
+	Copyright (C) 2021  Bishop Fox
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import (
 	"bytes"
 	"context"
@@ -37,7 +55,7 @@ func SelectBeacon(con *console.SliverConsoleClient) (*clientpb.Beacon, error) {
 		beaconsMap[beacon.ID] = beacon
 	}
 	keys := []string{}
-	for beaconID, _ := range beaconsMap {
+	for beaconID := range beaconsMap {
 		keys = append(keys, beaconID)
 	}
 	sort.Strings(keys)
@@ -62,7 +80,7 @@ func SelectBeacon(con *console.SliverConsoleClient) (*clientpb.Beacon, error) {
 	options := strings.Split(outputBuf.String(), "\n")
 	options = options[:len(options)-1] // Remove the last empty option
 	prompt := &survey.Select{
-		Message: "Select a session:",
+		Message: "Select a beacon:",
 		Options: options,
 	}
 	selected := ""
