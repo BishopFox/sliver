4b4e7c82f2abb0cb2451f74a579538679214823b
moloch-- <875022+moloch--@users.noreply.github.com>
2022-03-20 11:44:25 -0500

    Implant compiles, but errors on runtime

--- server/generate/binaries.go
+++ server/generate/binaries.go
@@ -644,7 +644,7 @@ func renderSliverGoCode(name string, config *models.ImplantConfig, goConfig *gog
 		return "", err
 	}
 	buildLog.Debugf("Created %s", goModPath)
-	output, err := gogo.GoMod((*goConfig), sliverPkgDir, []string{"tidy"})
+	output, err := gogo.GoMod((*goConfig), sliverPkgDir, []string{"tidy", "-compat=1.17"})
 	if err != nil {
 		buildLog.Errorf("Go mod tidy failed:\n%s", output)
 		return "", err
