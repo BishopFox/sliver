ff503203e3ca6591501e532579ae339b34750f41
RafBishopFox <rmarconi@bishopfox.com>
2022-04-27 13:45:28 -0400

    Changing token owner to token user to display user name associated with a process in Windows

--- implant/sliver/ps/ps_windows.go
+++ implant/sliver/ps/ps_windows.go
@@ -104,7 +104,7 @@ func getInfo(t syscall.Token, class uint32, initSize int) (unsafe.Pointer, error
 
 // getTokenOwner retrieves access token t owner account information.
 func getTokenOwner(t syscall.Token) (*syscall.Tokenuser, error) {
-	i, e := getInfo(t, syscall.TokenOwner, 50)
+	i, e := getInfo(t, syscall.TokenUser, 50)
 	if e != nil {
 		return nil, e
 	}
