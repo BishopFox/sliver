3351ab0fee1c79b7beaedd014cf7380206392380
rkervella <rkervella@bishopfox.com>
2022-08-25 14:24:16 -0700

    Fix implant vendor

--- implant/scripts/update-vendor.sh
+++ implant/scripts/update-vendor.sh
@@ -20,8 +20,8 @@ go mod tidy -compat=1.17
 go mod vendor
 
 # move updated files back
-mv "${tempDir}/vendor" ../../vendor
-mv "${tempDir}/go.mod" ../../go-mod
-mv "${tempDir}/go.sum" ../../go-sum
+mv "${tempDir}/vendor" "${pwd}/../vendor"
+mv "${tempDir}/go.mod" "${pwd}/../go-mod"
+mv "${tempDir}/go.sum" "${pwd}/../go-sum"
 cd ..
 rm -rf "$tempDir"
