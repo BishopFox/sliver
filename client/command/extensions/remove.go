24606aaa8fbc96efd2a72e84e0619b4c010cb1e0
rkervella <rkervella@bishopfox.com>
2022-08-29 10:16:17 -0700

    Remove commented code

--- client/command/extensions/remove.go
+++ client/command/extensions/remove.go
@@ -48,26 +48,3 @@ func ExtensionsRemoveCmd(ctx *grumble.Context, con *console.SliverConsoleClient)
 		con.PrintInfof("Extension '%s' removed\n", name)
 	}
 }
-
-// // RemoveExtensionByCommandName - Remove an extension by command name
-// func RemoveExtensionByCommandName(commandName string, con *console.SliverConsoleClient) error {
-// 	if commandName == "" {
-// 		return errors.New("command name is required")
-// 	}
-// 	if _, ok := loadedExtensions[commandName]; !ok {
-// 		return errors.New("extension not loaded")
-// 	}
-// 	delete(loadedExtensions, commandName)
-// 	con.App.Commands().Remove(commandName)
-// 	extPath := filepath.Join(assets.GetExtensionsDir(), filepath.Base(commandName))
-// 	if _, err := os.Stat(extPath); os.IsNotExist(err) {
-// 		return nil
-// 	}
-// 	forceRemoveAll(extPath)
-// 	return nil
-// }
-
-// func forceRemoveAll(rootPath string) {
-// 	util.ChmodR(rootPath, 0o600, 0o700)
-// 	os.RemoveAll(rootPath)
-// }
