608de5213a4259b785efce50cf878ee9af1a95cb
moloch-- <875022+moloch--@users.noreply.github.com>
2022-09-26 18:40:29 -0700

    Update wg GOGARBLE mask

--- server/generate/binaries.go
+++ server/generate/binaries.go
@@ -910,9 +910,8 @@ func getGoHttpsProxy() string {
 }
 
 const (
-	// GoPrivate - The default Go private arg to garble when obfuscation is enabled.
-	// Wireguard dependencies prevent the use of wildcard github.com/* and golang.org/*.
-	// The current packages below aren't definitive and need to be tidied up.
+	// The wireguard garble bug appears to have been fixed.
+	// Updated the wgGoPrivate to "*"
 	wgGoPrivate  = "*"
 	allGoPrivate = "*"
 )
