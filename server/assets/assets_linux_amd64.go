28ba6d5d5e5ad7abb8645ab0b3fd0f2c2fbf2375
rkervella <rkervella@bishopfox.com>
2022-12-02 14:20:49 -0800

    Remove old path

--- server/assets/assets_linux_amd64.go
+++ server/assets/assets_linux_amd64.go
@@ -5,6 +5,6 @@ package assets
 import "embed"
 
 var (
-	//go:embed fs/sliver.asc fs/libreflect.a fs/*.txt fs/*.zip fs/linux/amd64/* fs/lib/*.a
+	//go:embed fs/sliver.asc fs/*.txt fs/*.zip fs/linux/amd64/* fs/lib/*.a
 	assetsFs embed.FS
 )
