31930197d6a130a95da6ca3ab526314e24e068c7
moloch-- <875022+moloch--@users.noreply.github.com>
2023-02-01 15:16:48 -0800

    Remove manual seed of math/rand

--- implant/sliver/sliver.go
+++ implant/sliver/sliver.go
@@ -25,8 +25,6 @@ import "C"
 // {{end}}
 
 import (
-	"crypto/rand"
-	"encoding/binary"
 	"log"
 	insecureRand "math/rand"
 	"os"
@@ -67,13 +65,6 @@ var (
 )
 
 func init() {
-	buf := make([]byte, 8)
-	n, err := rand.Read(buf)
-	if err != nil || n != len(buf) {
-		insecureRand.Seed(time.Now().Unix())
-	} else {
-		insecureRand.Seed(int64(binary.LittleEndian.Uint64(buf)))
-	}
 	id, err := uuid.NewV4()
 	if err != nil {
 		buf := make([]byte, 16) // NewV4 fails if secure rand fails
