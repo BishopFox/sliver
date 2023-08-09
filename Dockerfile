5a532f77dcefa4828ddf8701826cd8e77e3c1c25
moloch-- <875022+moloch--@users.noreply.github.com>
2023-08-09 15:58:36 -0700

    Go v1.21.0

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.20.7
+FROM golang:1.21
 
 #
 # IMPORTANT: This Dockerfile is used for testing, I do not recommend deploying
--- go-assets.sh
+++ go-assets.sh
@@ -20,8 +20,8 @@ set -e
 
 # Creates the static go asset archives
 
-GO_VER="1.20.7"
-GARBLE_VER="1.20.7"
+GO_VER="1.21.0"
+GARBLE_VER="1.21.0"
 SGN_VER="0.0.3"
 
 BLOAT_FILES="AUTHORS CONTRIBUTORS PATENTS VERSION favicon.ico robots.txt SECURITY.md CONTRIBUTING.md LICENSE README.md ./doc ./test ./api ./misc"
