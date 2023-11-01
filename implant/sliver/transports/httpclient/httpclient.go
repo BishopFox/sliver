070bac1bf3f487f2b5ea463ff8b561d98d0cd210
Raf <84349012+RafBishopFox@users.noreply.github.com>
2023-11-01 12:52:42 -0400

    Fixing issue #1444: making User Agent a string literal in generation code

--- implant/sliver/transports/httpclient/httpclient.go
+++ implant/sliver/transports/httpclient/httpclient.go
@@ -48,7 +48,7 @@ import (
 var (
 	goHTTPDriver = "go"
 
-	userAgent      = "{{GenerateUserAgent}}"
+	userAgent      = `{{GenerateUserAgent}}`
 	nonceQueryArgs = "{{.HTTPC2ImplantConfig.NonceQueryArgs}}" // "abcdefghijklmnopqrstuvwxyz"
 
 	ErrClosed                             = errors.New("http session closed")
