f92067ef7d13b07c6c9a2043995855990627e97b
moloch-- <875022+moloch--@users.noreply.github.com>
2023-12-13 18:25:15 -0600

    Place holders

--- docs/sliver-docs/pages/docs/index.tsx
+++ docs/sliver-docs/pages/docs/index.tsx
@@ -3,8 +3,7 @@ import { NextPage } from "next";
 const IndexPage: NextPage = () => {
   return (
     <div>
-      <h1>Welcome to the Docs!</h1>
-      <p>This is the index page of the documentation.</p>
+      <p>Coming soon...</p>
     </div>
   );
 };
