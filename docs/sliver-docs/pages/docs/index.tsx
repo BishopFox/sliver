c16737bfe9277a36168ead5bc1a689de2f55c625
moloch-- <875022+moloch--@users.noreply.github.com>
2023-12-14 16:40:29 -0600

    Implement docs.json scheme

--- docs/sliver-docs/pages/docs/index.tsx
+++ docs/sliver-docs/pages/docs/index.tsx
@@ -1,11 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
+import React from "react";
 
-const IndexPage: NextPage = () => {
+type Doc = {
+  name: string;
+  content: string;
+};
+
+type Docs = {
+  docs: Doc[];
+};
+
+const DocsIndexPage: NextPage = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["docs"],
+    queryFn: async (): Promise<Docs> => {
+      const res = await fetch("/docs.json");
+      return res.json();
+    },
+  });
+
+  const docsMap = React.useMemo(() => {
+    if (!data) {
+      return null;
+    }
+    const docsMap = new Map<string, string>();
+    data.docs.forEach((doc) => {
+      docsMap.set(doc.name, doc.content);
+    });
+    return docsMap;
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <p>Coming soon...</p>
+      <h1>Docs Index</h1>
+      <ul>
+        {data?.docs.map((doc) => (
+          <li key={doc.name}>
+            <a href={`/docs/${doc.name}`}>{doc.name}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default IndexPage;
+export default DocsIndexPage;
