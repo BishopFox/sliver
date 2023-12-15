84c4832ff42dabb8f7e7254f6161cf7f09d8eab7
moloch-- <875022+moloch--@users.noreply.github.com>
2023-12-14 18:18:01 -0600

    Basic renderer working

--- docs/sliver-docs/pages/docs/index.tsx
+++ docs/sliver-docs/pages/docs/index.tsx
@@ -1,7 +1,16 @@
-import CodeViewer, { CodeSchema } from "@/components/code";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Listbox,
+  ListboxItem,
+} from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
 import React from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 type Doc = {
   name: string;
@@ -21,10 +30,12 @@ const DocsIndexPage: NextPage = () => {
     },
   });
 
+  const [name, setName] = React.useState(docs?.docs[0].name || "");
   const [markdown, setMarkdown] = React.useState(docs?.docs[0].content || "");
 
   React.useEffect(() => {
     if (docs) {
+      setName(docs.docs[0].name);
       setMarkdown(docs.docs[0].content);
     }
   }, [docs]);
@@ -35,24 +46,44 @@ const DocsIndexPage: NextPage = () => {
 
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-1"></div>
-      <div className="col-span-10">
-        <h1>Docs Index</h1>
-        <div>
-          <CodeViewer
-            className="min-h-[150px]"
-            script={
-              {
-                name: "",
-                script_type: "plaintext",
-                source_code: "asf",
-              } as CodeSchema
-            }
-          />
+      <div className="col-span-2 mt-6 ml-4">
+        <div className="flex flex-row justify-center text-lg mb-2 gap-2">
+          Sliver Docs
         </div>
-        <div></div>
+        <div className="mt-2">
+          <Listbox
+            aria-label="Toolbox Menu"
+            className="p-0 gap-0 divide-y divide-default-300/50 dark:divide-default-100/80 bg-content1 overflow-visible shadow-small rounded-medium"
+            itemClasses={{
+              base: "px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-default-100/80",
+            }}
+          >
+            {docs.docs.map((doc) => (
+              <ListboxItem
+                key={doc.name}
+                value={doc.name}
+                onClick={() => {
+                  setName(doc.name);
+                  setMarkdown(doc.content);
+                }}
+              >
+                {doc.name}
+              </ListboxItem>
+            ))}
+          </Listbox>
+        </div>
       </div>
-      <div className="col-span-1"></div>
+      <div className="col-span-10">
+        <Card className="mt-4 ml-4 mr-4 mb-4">
+          <CardHeader>
+            <span className="text-3xl">{name}</span>
+          </CardHeader>
+          <Divider />
+          <CardBody className="prose prose-slate">
+            <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 };
--- docs/sliver-docs/tailwind.config.ts
+++ docs/sliver-docs/tailwind.config.ts
@@ -10,31 +10,35 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100ch', // add required value here
+          }
+        }
+      }
     },
   },
   darkMode: "class",
-  plugins: [nextui({
-    prefix: "nextui", // prefix for themes variables
-    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-    defaultTheme: "light", // default theme from the themes object
-    defaultExtendTheme: "light", // default theme to extend on custom themes
-    layout: {}, // common layout tokens (applied to all themes)
-    themes: {
-      light: {
-        layout: {}, // light theme layout tokens
-        colors: {}, // light theme colors
+  plugins: [
+    require('@tailwindcss/typography'),
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {}, // light theme colors
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {}, // dark theme colors
+        },
+        // ... custom themes
       },
-      dark: {
-        layout: {}, // dark theme layout tokens
-        colors: {}, // dark theme colors
-      },
-      // ... custom themes
-    },
-  }),],
+    }),],
 }
 export default config
