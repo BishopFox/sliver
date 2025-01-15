c9a624831aff0ab19e1a715773882fe6902bf5c9
c2biz <ccardio@bishopfox.com>
2025-01-15 09:23:02 -0500

    fix input bug to AllowedName in GenerateExternal()

--- server/rpc/rpc-generate.go
+++ server/rpc/rpc-generate.go
@@ -340,7 +340,7 @@ func (rpc *Server) GenerateExternal(ctx context.Context, req *clientpb.ExternalG
 		if err != nil {
 			return nil, err
 		}
-	} else if err := util.AllowedName(name); err != nil {
+	} else if err := util.AllowedName(req.Name); err != nil {
 		return nil, err
 	} else {
 		name = req.Name
