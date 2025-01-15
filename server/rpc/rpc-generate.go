ed64f2e0d22fb1e1c8aa9e3e548e3f99caed9d44
c2biz <ccardio@bishopfox.com>
2025-01-15 08:40:45 -0500

    fix bug ignoring supplied name for generate functions

--- server/rpc/rpc-generate.go
+++ server/rpc/rpc-generate.go
@@ -68,7 +68,7 @@ func (rpc *Server) Generate(ctx context.Context, req *clientpb.GenerateReq) (*cl
 		if err != nil {
 			return nil, err
 		}
-	} else if err := util.AllowedName(name); err != nil {
+	} else if err := util.AllowedName(req.Name); err != nil {
 		return nil, err
 	} else {
 		name = req.Name
