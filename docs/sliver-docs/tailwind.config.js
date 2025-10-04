b07e750cf50c5e2b75384dbf660d75fdedc393ab
moloch-- <875022+moloch--@users.noreply.github.com>
2025-10-04 15:06:19 -0700

    Add missing tailwind.config.js

---
+++ docs/sliver-docs/tailwind.config.js
@@ -0,0 +1,22 @@
+import {heroui} from "@heroui/theme"
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
+
+module.exports = config;
