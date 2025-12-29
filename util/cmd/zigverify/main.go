205f5b41960fff1ac2c70986b2d84f4b9c182d8a
moloch-- <875022+moloch--@users.noreply.github.com>
2025-12-29 12:04:27 -0600

    Update zig download to be more polite and secure issue #2065

--- util/cmd/zigverify/main.go
+++ util/cmd/zigverify/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 
 	minisign "github.com/bishopfox/sliver/util/minisign"
 )
@@ -26,7 +27,7 @@ func loadPublicKey() (minisign.PublicKey, error) {
 	return publicKey, nil
 }
 
-func verifySignature(artifactPath, signaturePath string) error {
+func verifySignature(artifactPath, signaturePath, expectedFile string) error {
 	artifact, err := os.Open(artifactPath)
 	if err != nil {
 		return fmt.Errorf("open artifact: %w", err)
@@ -52,16 +53,46 @@ func verifySignature(artifactPath, signaturePath string) error {
 		return errors.New("signature verification failed")
 	}
 
+	if err := verifyTrustedComment(sigData, expectedFile); err != nil {
+		return err
+	}
+
 	return nil
 }
 
+func verifyTrustedComment(signature []byte, expectedFile string) error {
+	var sig minisign.Signature
+	if err := sig.UnmarshalText(signature); err != nil {
+		return fmt.Errorf("parse signature: %w", err)
+	}
+
+	fileField := trustedCommentFile(sig.TrustedComment)
+	if fileField == "" {
+		return fmt.Errorf("trusted comment missing file field: %q", sig.TrustedComment)
+	}
+	if fileField != expectedFile {
+		return fmt.Errorf("trusted comment file mismatch: expected %q, got %q", expectedFile, fileField)
+	}
+
+	return nil
+}
+
+func trustedCommentFile(trustedComment string) string {
+	for field := range strings.FieldsSeq(trustedComment) {
+		if after, ok := strings.CutPrefix(field, "file:"); ok {
+			return after
+		}
+	}
+	return ""
+}
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "usage: %s <artifact-path> <signature-path>\n", os.Args[0])
+	if len(os.Args) != 4 {
+		fmt.Fprintf(os.Stderr, "usage: %s <artifact-path> <signature-path> <expected-filename>\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	if err := verifySignature(os.Args[1], os.Args[2]); err != nil {
+	if err := verifySignature(os.Args[1], os.Args[2], os.Args[3]); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
