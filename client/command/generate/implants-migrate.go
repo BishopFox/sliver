6b7df921e30964672708ed818798c7a1b6e8c743
0xOral <110070707+skoveit@users.noreply.github.com>
2026-02-26 01:42:57 +0200

    typo

--- client/command/generate/implants-migrate.go
+++ client/command/generate/implants-migrate.go
@@ -2,7 +2,7 @@ package generate
 
 /*
 	Sliver Implant Framework
-	Copyright (C) 2019  Bishop Fox
+	Copyright (C) 2026  Bishop Fox
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
